4ca4308c3ca5d4a0c9c2e93ac5e23189c3afb277
irenecascio07-max <irenecascio07@gmail.com>
2026-06-23 14:32:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-function saluta() {
-alert("Ci sono per te ❤️");
-}
+[
+"Ci sono io per te ❤️",
+"Respira... sei al sicuro",
+"Un passo alla volta, ok?",
+"Meriti pace e calma oggi",
+"Non sei sola, promesso",
+"Chiudi gli occhi 3 secondi... ora va meglio?"
+];
 
-if ('serviceWorker' in navigator) {
-navigator.serviceWorker.register('sw.js');
-}
+document.getElementById("btn").addEventListener("click", () => {
+const random = frasi[Math.floor(Math.random() * frasi.length)];
+document.getElementById("testo").innerText = random;
+});
